b3c2d8070bcf947fd33710b7f161a27c4daffc3a
azael537 <azaelolivargarcia@gmail.com>
2021-10-06 21:20:02 -0500

    Envio tarea

--- js/index.js
+++ js/index.js
@@ -4,13 +4,15 @@
 1. Crea una función llamada palindromo() que reciba como parámetro una palabra, si la oración es un palíndromo la función deberá devolver true en otro caso, devolver false
 Extra: con oraciones
 */
+// R E C O N O C E R
 
-function palindromo(palabra){
+function palindromo(palabra) {
     let palabraVolteada = palabra.split("").reverse().join("");
-    if(palabra == palabraVolteada){
+
+    if (palabra == palabraVolteada) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
@@ -18,25 +20,19 @@ function palindromo(palabra){
 /*
 2. Crea una función llamada longestCountry(), esta función recibirá de parámetro un arreglo con nombres de distintos países, la función deberá retornar el país con mayor número de letras. 
 Toma como ejemplo, del siguiente arreglo de países:
-
 const paises = ["México", "Colombia", "Peru", "El Salvador"]
 */
 
 const paises = ["México", "Colombia", "Peru", "El Salvador"];
-
-function paisMasLargo(arregloDePaises){
-
+function paisMasLargo(arregloDePaises) {
     let paisLargo = "";
-    for(let i = 0; i < arregloDePaises.length; i++ ){
-
-        if(arregloDePaises[i].length > paisLargo.length){
-            paisLargo = arregloDePaises[i];
-        }
+    for (let index = 0; index < arregloDePaises.length; index++) {
+        if (paisMasLargo[index].length > paisLargo.length){
+            paisLargo = arregloDePaises[index];
+        } 
     }
     return paisLargo;
-
 }
-
 
 
 /*
@@ -52,18 +48,9 @@ farenheitToCelcius(55);
 /*
 4. Crea una función que imprima el número de parámetros enviados.
 */
-array = ["hola", 78, false, 4.56];
+array = ["hola", 78, false, 4.56, "Salvador es muy malo"];
 
 function spreadOperator(...array) {
     return spreadOperator;
 }
-spreadOperator(0,3);
-
-
-function suma(x,y,z){
-    return x + y + z;
-}
-const numbers = [1, 2, 3];
-suma(numbers);
-
-
+spreadOperator(0,5);
